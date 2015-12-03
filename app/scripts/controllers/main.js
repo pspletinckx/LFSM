6594745ffee51dcffdd305f0a773cd583fb15e73
Pieter Pletinckx <pieplay@gmail.com>
2015-12-03 01:51:46 +0100

    caching for a very fat algorithm

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,18 @@
  * Controller of the highTechRecruitmentApp
  */
 angular.module('highTechRecruitmentApp')
-  .controller('MainCtrl',['$scope', 'daybreakCharacter', function ($scope,daybreakCharacter) {
+  .controller('MainCtrl',['$scope', 'daybreakCharacter','$cacheFactory', function ($scope,daybreakCharacter,$cacheFactory) {
+  	var startBR =100;
+  	var endBR = 56;
+  	$scope.progress = 40;
+
+  	var doneBr = function(){
+  		var increment = 100 / (startBR-endBR);
+  		$scope.progress + increment;
+  	}
+  	$scope.cache = $cacheFactory('knownKandidates');
+
+  	
   	$scope.kandidates = [];
   	// kandidate name, battlerank, online, last played
   	 	var nextPlayer = function(i){
@@ -27,13 +38,25 @@ angular.module('highTechRecruitmentApp')
 				  				{
 				  					name: character.name.first,
 				  					id: character.character_id,
-				  					br: character.battle_rank.value
+				  					br: character.battle_rank.value,
+				  					online: character.online_status
 				  				});
 				  		}
 			  			console.log(data.data.returned);//1111111
-			  			if (data.data.returned == 0) i=-1;
+			  			if (data.data.returned == 0) {
+			  				i=-1;
+			  				$scope.cache.put(100,kandidates);
+			  				doneBr();
+
+			  			}
 			  		},
 			  		function(){});
  			  };
  		nextPlayer(1); //start
+ 		doneBr();
+ 		doneBr();
+ 		doneBr();
+ 		doneBr();
+ 		doneBr();
+
   }]);
